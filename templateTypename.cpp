a1c65d6666ccba95967e25fd07e58200dd6b1b6b
Hyunsoo Lee <96625665+ahyunsoo3@users.noreply.github.com>
2026-02-11 00:37:18 +0900

    Create templateTypename.cpp

---
+++ templateTypename.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+template<typename T>
+T add(T a, T b) {
+    return a + b;
+}
+
+int main() {
+    int x = 3, y = 5;
+    std::cout << add(x, y) << std::endl;
+
+    double a = 2.5, b = 1.5;
+    std::cout << add(a, b) << std::endl;
+
+    std::string s1 = "Hello, ";
+    std::string s2 = "World";
+    std::cout << add(s1, s2) << std::endl;
+
+    return 0;
+}
+
+/* With this template, we don't have to write the type explicitly (e.g., add<int>, add<std:string>)
+The compiler can deduce T from arguments.
+*/
